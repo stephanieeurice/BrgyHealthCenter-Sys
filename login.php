60eb148370af80a445a0ec62e6e57f4d33887d36
Alondra Borja <alondracorbillonborjaborja14.ab@gmail.com>
2022-02-09 09:01:56 +0800

    edited some styles

--- login.php
+++ login.php
@@ -11,10 +11,11 @@
     <link href="assets/custom/login.css" rel="stylesheet">
     <link href="assets/custom/heroes.css" rel="stylesheet">
     <link href="assets/custom/alert2.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awFesome/4.7.0/css/font-awesome.min.css">
   </head>
 
-  <body class="text-center" style="background-image: linear-gradient(145deg, #23467a , #36a7e3); background-size: 500px; background-repeat: no-repeat;">
-
+  <body class="text-center" style="">
     <!-- Alerts -->
       <?php
         if (isset($_GET["error"])) {
@@ -50,57 +51,126 @@
                 </div>";
         }
         ?>
-
-          <div class="container rounded-3 " style="box-shadow: 0 7px 50px 0 rgba(0, 0, 0, 0.29)">
-                  
-                  <div class="row center-align">
-                      
-                      <div class="col-6 col-md-4 rounded-3" style="height: 500px">
-                        <img src="assets/logo/temp.png" width="50%" style="margin:80px 30px 0px 0px">
-                          <p class="py-4" style="margin-right:35px; text-align: center; color: white;line-height: 1.3; font-size: 15px">Brgy. Health Center App<br>Online Health Consultation Portal</p>
-                      </div>
-
-                      <div class="col-sm-6 col-md-8" >
-
-                          <main class="form-signin" style="">
-
-                              <!-- Form -->
-
-                              <form action="includes/login.inc.php" method="post">
-                                
-                                <h2 class="h4 mb-3 fw-normal" style="margin-top: 40px">Log In</h2>
-
-                                <div class="form-floating">
-                                  <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
-                                  <label for="email">Email</label>
-                                </div>
-                                <div class="form-floating">
-                                  <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-                                  <label for="password">Password</label>
-                                </div>
-                                <div>
-                                  <select class="form-select" id="auth" name="auth">
-                                    <option value="Patient" selected>Patient</option>
-                                    <option value="Doctor">Doctor</option>
-                                    <option value="Admin">Admin</option>
-                                  </select>
-                                </div>
-
-                                <div class="checkbox mb-3" style="margin:20px">
-                                  <label>
-                                    <input type="checkbox" value="lsRememberMe" id="rememberMe"> Remember me
-                                  </label>
-
-                                </div>
-                                <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit" onclick="lsRememberMe()" >Sign in</button>
-                                <a href="register.php" style="text-decoration: none;"><button class="w-100 btn btn-lg btn-secondary" style="margin-top:5px;color:black" type="button">Register</button></a>
-                              </form>
-                          </main>
-                    </div>
-
-                </div>
+    
+    <header>
+      <div class="logo">
+        <a href="index.php">     
+          <img src="assets/logo/temp.png" alt="Website logo" height="55" style="margin-bottom: 6px; padding-right: 0">
+        </a>
+        <div class='app-name' style="margin-top: 5px; text-align: left;">
+          <a href="index.php">
+            <span>Community Health</span><br>
+            <span>Center App</span>
+          </a>
+        </div> 
       </div>
+      <span class="divider"></span>
+      <div class="login-btn">
+        <a href="login.php"><button>Login</button></a>
+      </div>      
+    </header>  
+      
+    <div class="container rounded-3" style="margin-top: 5%; margin-bottom: 5%; width: 70%; background-image: linear-gradient(145deg, #23467a , #36a7e3); background-size: 30%; background-repeat: no-repeat; box-shadow: 0 7px 50px 0 rgba(0, 0, 0, 0.29);">
+                
+      <div class="row center-align" >
+                
+        <div class="col-6 col-md-4 rounded-3" style="height: 500px; width: 30%;">
+          <img src="assets/logo/temp.png" width="50%" style="margin:80px 0 0 0">
+            <p class="app-long-name py-4" style="text-align: center; color: white;line-height: 1.3; font-size: 15px;">Community Health Center App<br>Online Health Consultation Portal</p>
+        </div>
 
+        <div class="col-sm-6 col-md-8" style="width: 70%;">
+
+          <main class="form-signin" style="">
+
+              <!-- Form -->
+
+              <form action="includes/login.inc.php" method="post">
+                
+                <h3 class="h3 mb-3 fw-normal" style="margin-top: 40px; color: #23467a;">Login</h3>
+
+                <div class="inputs" style="margin-top: 40px;">
+                  <label for="email">Email</label>
+                  <input type="text" class="form-control" id="email" name="email" placeholder="someone@email.com" required>                                    
+                </div>
+                <div class="inputs">
+                  <label for="password">Password</label>
+                  <input type="password" class="form-control" id="password" name="password" placeholder="Please enter your password" required>
+                </div>
+                <div style="margin-top: 30px;">
+                  <select class="form-select" id="auth" name="auth">
+                    <option value="Patient" selected>Patient</option>
+                    <option value="Doctor">Doctor</option>
+                    <option value="Admin">Admin</option>
+                  </select>
+                </div>
+
+                <div class="others">
+                  <div class="checkbox mb-3">
+                    <label>
+                      <input type="checkbox" value="lsRememberMe" id="rememberMe"> Remember me
+                    </label>
+                  </div>
+                  <div class="checkbox mb-3">
+                    <a href="">Forgot Password?</a>
+                  </div>
+                </div>
+                
+                <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit" onclick="lsRememberMe()" >Login</button>
+                
+              </form>
+              <div class="divider-msg">Or login with</div>
+              <div class="app-buttons">
+                  <button class="facebook facebook-l" style="margin-right: 10px;"><i class="fa fa-facebook-square"></i><span>Facebook</span></button>
+                  <button class="google google-l"><i><img src="assets/icons/google.png" alt="Google logo" width="20px"></i><span>Google</span></button>
+              </div>
+              <div style="margin-bottom: 30px;">Don't have an account?&nbsp;&nbsp;<a href="register.php">Register here</a></div>
+              <!-- inside nung <a href=""></a>
+              <button class="w-100 btn btn-lg btn-secondary" style="margin-top:5px;color:black" type="button"></button> -->
+          </main>
+        </div>
+
+      </div>
+    </div>
+
+    <footer class="footer">
+      <div class="footer-left">
+        <div class="logo">
+          <a href="index.php">     
+            <img src="assets/logo/temp.png" alt="Website logo" style="margin: 0 0 15px -15px; padding-right: 0">
+          </a>
+          <div style="margin-top: 15px; text-align: left;">
+            <a href="index.php">
+            <span class="up">Community Health</span><br>
+            <span>Center App</span>
+            </a>
+          </div>
+        </div>          
+      </div>
+      <ul class="footer-right" style="text-align: left;">
+        <li>
+          <h3>Quick Links</h3>
+          <ul class="box">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms and Conditions</a></li>
+          </ul>
+        </li>
+        <li>
+          <h3>Contact</h3>
+          <ul class="box">
+            <li><a href="#">Address here...</a></li>
+            <li><a href="#">Address here...</a></li>
+            <li><a href="#"><i class="fa fa-phone-square"></i>&nbsp;Phone: 0912 345 6789</a></li>
+            <li><a href="#"><i class="fa fa-envelope"></i>&nbsp;Email: communityhealthcenterapp@gmail.com</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div class="footer-bottom">
+        <p>All rights reserved by &copy; Community Health Center App 2022</p>
+      </div>
+    </footer>
   </body>
 
   <script type="text/javascript">
--- register.php
+++ register.php
@@ -10,6 +10,8 @@
     <link href="assets/custom/login.css" rel="stylesheet">
     <link href="assets/custom/heroes.css" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <style>
 
       .bd-placeholder-img {
@@ -46,54 +48,66 @@
     </style>
   </head>
 
-  <body class="text-center" style="background-image: linear-gradient(145deg, #23467a , #36a7e3); background-size: 100%; background-repeat: no-repeat;">
-      <div class="container rounded-3 g-5" style="box-shadow: 0 7px 50px 0 rgba(0, 0, 0, 0.29); height: 600px; background-color:#F2F2F2">
+  
+  <body>
+    <header>
+      <div class="logo">
+        <a href="index.php">     
+          <img src="assets/logo/temp.png" alt="Website logo" height="55" style="margin-bottom: 6px; padding-right: 0">
+        </a>
+        <div style="margin-top: 5px;">
+          <a href="index.php">
+            <span class="up">Community Health</span><br>
+            <span>Center App</span>
+          </a>
+        </div>
+      </div>
+      <span class="divider"></span>
+      <div class="login-btn">
+        <a href="login.php"><button>Login</button></a>
+      </div>      
+    </header>    
+
+      <div class="form-container container rounded-3 g-5" style="margin-top: 5%; margin-bottom: 5%; width: 70%; box-shadow: 0 7px 50px 0 rgba(0, 0, 0, 0.29); min-height: 640px;">
                   
-          <div class="row container-fluid">
-              <div>  
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">  
-                  <h2 style="margin-bottom: 15px">Register</h2>
-                  <div class="btn-toolbar mb-2 mb-md-0 row">
-                    <h6 style="margin-left:-50px;margin-top:20px">Login with:</h6>
-                    <div class="btn-group me-2">
-                      <img src="assets/icons/fb.png" height="50px">
-                      <img src="assets/icons/google.png" height="50px" style="margin-left:10px">
-                    </div>
-                  </div>
+          <div class="row container-fluid py-4">
+              <h2 style="color: #23467a;">Register</h2>
+              <div class="d-flex justify-content-between align-items-end flex-wrap">           
+                <!-- Select Classification if Patient/Doctor then show fields of which was selected-->
+                <div class="col-3 select" style="width: 240px;"> 
+                    <select class="form-select" id="classification" onchange="selectChange(event)">
+                          <option value="0" disabled selected>Select your classification</option> 
+                          <option value="1">Patient</option>
+                          <option value="2">Doctor</option>
+                    </select>
                 </div>
 
-      <!-- Select Classification if Patient/Doctor then show fields of which was selected-->
+                <div class="app-buttons-reg"  >
+                  Or register with:
+                  <div style="padding-top: 5px;">
+                  <button class="facebook" style="padding: 0 20px 0 5px; margin-right: 5px; width: 140px;"><i class="fa fa-facebook-square" style="margin: 10px"></i><span>Facebook</span></button>
+                  <button class="google" style="padding: 0 20px 0 0; width: 140px; height: 42px;"><img src="assets/icons/google.png" alt="Google logo" width="17px" style="margin: 0 10px 3px 0"><span>Google</span></button>
 
-                  <div class="col-3" style="margin-top: -20px"> 
-                      <select class="form-select" id="classification" onchange="selectChange(event)">
-                            <option value="0" disabled selected>Select your classification</option> 
-                            <option value="1">Patient</option>
-                            <option value="2">Doctor</option>
-                      </select>
                   </div>
+                </div>
 
               </div>
 
               <hr class="my-4">
 
-      <!-- Patient Registration Form -->
+              <!-- Patient Registration Form -->
 
               <div id="patient" hidden="true">
               <form action="includes/register_patient.inc.php" method="post">
 
-                  <div class="row g-3" >
+                  <div class="row g-2 main-form">
 
-                      <div class="col-sm-4">
+                      <div class="col-sm-9 user-inputs">
                         <label for="name" class="form-label">Full Name</label>
-                        <input type="text" class="form-control" id="name" name="name" required>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="ex. Juan Dela Cruz" required>
                       </div>
 
-                      <div class="col-4">
-                        <label for="birthday" class="form-label">Date of Birth</label>
-                        <input type="date" class="form-control" id="birthday" name="birthday" required >
-                      </div>
-
-                      <div class="col-4">
+                      <div class="col-3 user-inputs">
                         <label for="gender" class="form-label">Gender</label>
                         <select class="form-select" required name="gender" id="gender">
                             <option value="Male" selected>Male</option>
@@ -101,34 +115,48 @@
                         </select>
                       </div>
 
-                      <div class="col-9">
-                        <label for="address" class="form-label">Address</label>
-                        <textarea class="form-control" name="address" id="address" style="resize:none; height:20px" required></textarea>
+                      <div class="col-5 user-inputs">
+                        <label for="birthday" class="form-label">Date of Birth</label>
+                        <input type="date" class="form-control" id="birthday" name="birthday" required >
                       </div>
 
-                      <div class="col-3">
+                      <div class="col-3 user-inputs">
+                        <label for="senior-citizen" class="form-label">Are you a senior citizen?</label><br>
+                        <div style="display: flex;">
+                          <input type="radio" name="radio" class="options" required >
+                          <div style="margin-top: 8px;"><label for="senior-citizen" class="form-label">Yes</label></div>
+                          <input type="radio" name="radio" class="options" style="margin-left: 20px;" required >
+                          <div style="margin-top: 8px;"><label for="senior-citizen" class="form-label" style="" >No </label></div>
+                        </label>
+                        </div>  
+                      </div>
+
+                      <div class="col-4 user-inputs">
                         <label for="contactnum" class="form-label">Contact Number</label>
-                        <input type="text" class="form-control" id="contactnum" name="contactnum" required >
+                        <input type="text" class="form-control" id="contactnum" name="contactnum" placeholder="09XX XXX XXXX" required >
                       </div>
 
-                      <div class="col-sm-6">
+                      <div class="col-12 user-inputs">
+                        <label for="address" class="form-label">Address</label>
+                        <textarea class="form-control" name="address" id="address" style="resize:none; height:20px" placeholder="Please enter your full address" required></textarea>
+                      </div>
+
+                      <div class="col-sm-6 user-inputs">
                         <label for="email" class="form-label">Email Address</label>
-                        <input type="text" class="form-control" id="email" name="email" required>
+                        <input type="text" class="form-control" id="email" name="email" placeholder="ex.juandelacruz@gmail.com" required>
                       </div>
 
-                      <div class="col-6">
+                      <div class="col-6 user-inputs">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required >
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Please enter your password" required >
                       </div>
 
                       <hr class="my-4">
 
-                      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap ">  
-                          <div style="display: flex; justify-content:left; align-items: left">
-                            <a href="login.php" style="text-decoration: none;"><button class=" btn btn-secondary btn-lg" type="button" style="width: 100px">Back</button></a>
-                          </div>
-
-                          <div style="display: flex; justify-content:right; align-items: right">
+                      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap form-bottom" style="margin-top: 5px;">
+                          <div class="to-login-container" style="display: flex; justify-content:left; align-items: left; margin-top: 30px;">Already have an account?&nbsp;&nbsp;<a href="login.php">Login here</a></div>  
+                          
+                          <div style="display: flex; justify-content:right; align-items: right;">
                             <button class=" btn btn-primary btn-lg" type="submit" name="submit" style="width: 200px">Register</button>
                           </div>
                       </div>
@@ -137,24 +165,19 @@
                 </form>
                 </div>
 
-      <!-- Doctor Registration Form -->
+                <!-- Doctor Registration Form -->
 
                 <div id="doctor" hidden="true">
                 <form action="includes/register_doctor.inc.php" method="post">
 
-                  <div class="row g-3" >
+                  <div class="row g-2 main-form">
 
-                      <div class="col-sm-4">
+                      <div class="col-sm-9 user-inputs">
                         <label for="name" class="form-label">Full Name</label>
-                        <input type="text" class="form-control" id="name" name="name" required>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="ex. Juan Dela Cruz" required>
                       </div>
 
-                      <div class="col-4">
-                        <label for="birthday" class="form-label">Date of Birth</label>
-                        <input type="date" class="form-control" id="birthday" name="birthday" required >
-                      </div>
-
-                      <div class="col-4">
+                      <div class="col-3 user-inputs">
                         <label for="gender" class="form-label">Gender</label>
                         <select class="form-select" required name="gender" id="gender">
                             <option value="Male" selected>Male</option>
@@ -162,43 +185,46 @@
                         </select>
                       </div>
 
-                      <div class="col-9">
-                        <label for="address" class="form-label">Address</label>
-                        <textarea class="form-control" name="address" id="address" style="resize:none; height:20px" required></textarea>
-                      </div>
-
-                      <div class="col-3">
+                      <div class="col-4 user-inputs">
+                        <label for="birthday" class="form-label">Date of Birth</label>
+                        <input type="date" class="form-control" id="birthday" name="birthday" required >
+                      </div>  
+                      
+                      <div class="col-4 user-inputs">
                         <label for="contactnum" class="form-label">Contact Number</label>
-                        <input type="text" class="form-control" id="contactnum" name="contactnum" required >
+                        <input type="text" class="form-control" id="contactnum" name="contactnum" placeholder="09XX XXX XXXX" required >
                       </div>
 
-                      <div class="col-sm-4">
-                        <label for="email" class="form-label">Email Address</label>
-                        <input type="text" class="form-control" id="email" name="email" required>
-                      </div>
-
-                      <div class="col-4">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required >
-                      </div>
-
-                      <div class="col-4" style="margin-bottom: 20px">
+                      <div class="col-4 user-inputs" style="margin-bottom: 20px">
                         <label for="specialization" class="form-label">Specialization</label>
                         <select class="form-select" required name="specialization" id="specialization">
                             <option value="Pediatrician" selected>Pediatrician</option>
-                            <option value="Obgyn" >Obstetrician/Gynecologist</option>
+                            <option value="Obstetrician" >Obstetrician/Gynecologist</option>
                             <option value="Cardiologist" >Cardiologist</option>
                             <option value="Dentist" >Dentist</option>
                             <option value="Ophthalmologist" >Ophthalmologist</option>
                         </select>
                       </div>
 
+                      <div class="col-12 user-inputs" style="margin-top: -10px">
+                        <label for="address" class="form-label">Address</label>
+                        <textarea class="form-control" name="address" id="address" style="resize:none; height:20px" placeholder="Please enter your full address" required></textarea>
+                      </div>                      
+
+                      <div class="col-6 user-inputs">
+                        <label for="email" class="form-label">Email Address</label>
+                        <input type="text" class="form-control" id="email" name="email" placeholder="ex.juandelacruz@gmail.com" required>
+                      </div>
+
+                      <div class="col-6 user-inputs"  style="margin-bottom: 14px">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Please enter your password" required >
+                      </div>                   
+
                       <hr class="my-2">
 
-                      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap ">  
-                          <div style="display: flex; justify-content:left; align-items: left">
-                            <a href="login.php" style="text-decoration: none;"><button class=" btn btn-secondary btn-lg" type="button" style="width: 100px">Back</button></a>
-                          </div>
+                      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap form-bottom" style="margin-top: 20px;">  
+                          <div class="to-login-container" style="display: flex; justify-content:left; align-items: left; margin-top: 30px;">Already have an account?&nbsp;&nbsp;<a href="login.php">Login here</a></div>  
 
                           <div style="display: flex; justify-content:right; align-items: right">
                             <button class=" btn btn-primary btn-lg" type="submit" name="submit" style="width: 200px">Register</button>
@@ -211,6 +237,44 @@
               </div>
 
       </div>
+    <footer class="footer">
+      <div class="footer-left">
+        <div class="logo">
+          <a href="index.php">     
+            <img src="assets/logo/temp.png" alt="Website logo" style="margin: 0 0 15px -15px; padding-right: 0">
+          </a>
+          <div style="margin-top: 15px;">
+            <a href="index.php">
+              <span class="up">Community Health</span><br>
+              <span>Center App</span>
+            </a>
+          </div>
+        </div>          
+      </div>
+      <ul class="footer-right">
+        <li>
+          <h3>Quick Links</h3>
+          <ul class="box">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms and Conditions</a></li>
+          </ul>
+        </li>
+        <li>
+          <h3>Contact</h3>
+          <ul class="box">
+            <li><a href="#">Address here...</a></li>
+            <li><a href="#">Address here...</a></li>
+            <li><a href="#"><i class="fa fa-phone-square"></i>&nbsp;Phone: 0912 345 6789</a></li>
+            <li><a href="#"><i class="fa fa-envelope"></i>&nbsp;Email: communityhealthcenterapp@gmail.com</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div class="footer-bottom">
+        <p>All rights reserved by &copy; Community Health Center App 2022</p>
+      </div>
+    </footer>  
   </body>
 
   <script type="text/javascript">

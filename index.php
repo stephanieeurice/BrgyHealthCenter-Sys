3c0cd54ccc535a6da30773e0d5459c96ee2d1568
Alondra Borja <alondracorbillonborjaborja14.ab@gmail.com>
2022-02-09 09:01:16 +0800

    welcome page of the website

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="assets/logo/temp.png" >
+    <title>Welcome</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/custom/welcome.css" rel="stylesheet">
+    <link href="assets/custom/heroes.css" rel="stylesheet">  
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  
+  <body>
+    <header>
+      <div class="logo">
+        <a href="index.php">     
+          <img src="assets/logo/temp.png" alt="Website logo" height="55" style="margin-bottom: 6px; padding-right: 0">
+        </a>
+        <div style="margin-top: 5px;">
+          <a href="index.php">
+            <span class="up">Community Health</span><br>
+            <span>Center App</span>
+          </a>
+        </div>  
+      </div>
+      <span class="divider"></span>
+      <div class="login-btn">
+        <a href="login.php"><button>Login</button></a>
+      </div>      
+    </header>    
+      <div class="get-started" style="background: linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)), url(assets/images/diagnosis.jpg); background-size: cover;">
+        <div class="get-started-div">
+          <h3 class="get-started-msg">
+            This app is an online consultation app dedicated for Community Health Centers. Users can 
+            access the app through the mobile app and the website. <br><br>
+            The system offers different services such as online consultation with a doctor or 
+            any health practitioner, schedule an appointment, chat or have a video call with your doctor and  
+            receive a prescription based on the consultation. <br><br>     
+            Learn more about the app and how it can help your healthcare needs. <br><br> 
+            Start using the app now! <br><br>
+          </h3>
+          <div id="get-started-btn">
+            <a href="register.php"><button>Get Started >></button></a>   
+          </div>
+        </div>
+      </div>
+      
+      <div class="second-div">
+
+      </div>
+
+      <footer class="footer">
+        <div class="footer-left">
+          <div class="logo">
+            <a href="index.php">     
+              <img src="assets/logo/temp.png" alt="Website logo" style="margin: 0 0 15px -15px; padding-right: 0">
+            </a>
+            <div style="margin-top: 15px;">
+              <a href="index.php">
+                <span class="up">Community Health</span><br>
+                <span>Center App</span>
+              </a>
+            </div>
+          </div>          
+        </div>
+        <ul class="footer-right">
+          <li>
+            <h3>Quick Links</h3>
+            <ul class="box">
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Terms and Conditions</a></li>
+            </ul>
+          </li>
+          <li>
+            <h3>Contact</h3>
+            <ul class="box">
+              <li><a href="#">Address here...</a></li>
+              <li><a href="#">Address here...</a></li>
+              <li><a href="#"><i class="fa fa-phone-square"></i>&nbsp;Phone: 0912 345 6789</a></li>
+              <li><a href="#"><i class="fa fa-envelope"></i>&nbsp;Email: communityhealthcenterapp@gmail.com</a></li>
+            </ul>
+          </li>
+        </ul>
+        <div class="footer-bottom">
+          <p>All rights reserved by &copy; Community Health Center App 2022</p>
+        </div>
+      </footer>
+
+  </body>
+
+</html>

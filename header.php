e40d061bc3e63eefc081a7c2a7d4f59fdc4a60fb
Alondra Borja <alondracorbillonborjaborja14.ab@gmail.com>
2022-02-09 09:00:53 +0800

    edited the style for header

--- header.php
+++ header.php
@@ -2,44 +2,30 @@
 	include 'includes/db.inc.php';
 ?>
 
-     <header class="p-3 border-bottom">
-          <div class="container">
-              <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="#" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-                  <img src="assets/logo/temp.png" height="40">
-                </a>
 
-                <h5 class="nav col-lg-auto me-lg-auto mb-2  mb-md-0 py-2">Online Health Consultation Portal</h5>
+  <header class="p-3 border-bottom" style="display: flex; background-color: #23467a; padding: 10px 5%; color: #ffffff;">
+    <div style="display: flex; flex: 1; cursor: pointer;font-size: 16px; margin: 0 20px 0 20px;">
+      <a href="patient_dashboard.php">     
+        <img src="assets/logo/temp.png" alt="Website logo" height="55" style="margin-bottom: 6px; padding-right: 0">
+      </a>
+      <div style="margin-top: 5px; text-align: left; font-size: 16px; font-weight: 500;">
+        <span>Online Health</span><br>
+        <span>Consultation Portal</span>
+      </div> 
+    </div>
+    <div class="dropdown text-end" style="margin-top: 20px;">
+      <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false" style="color: #fff">
+        <!--img src="https://emoji.gg/assets/emoji/8229_KannaPog.png" alt="" width="32" height="32" class="rounded-circle me-2"-->
+        <strong style="color: #fff"><?php echo $name = $_SESSION["usernm"]; ?></strong>
+      </a>
+      <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li  style="color:#23467a;"><a class="dropdown-item" href="login.php?logout">Sign out</a></li>
+      </ul>
+    </div>
 
-                <div class="dropdown text-end">
-                  <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <!--img src="https://emoji.gg/assets/emoji/8229_KannaPog.png" alt="" width="32" height="32" class="rounded-circle me-2"-->
-                    <strong style="color: #252626"><?php echo $name = $_SESSION["usernm"]; ?></strong>
-                  </a>
-                  <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                    <li><a class="dropdown-item" href="#">Settings</a></li>
-                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="login.php?logout">Sign out</a></li>
-                  </ul>
-                </div>
-
-                <!--div class="dropdown">
-                <a href="#" class="d-flex align-items-center link-light text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                  <img src="https://emoji.gg/assets/emoji/8229_KannaPog.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                  <strong style="color: white">Zyrah Avila</strong>
-                </a>
-                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-                  <li><a class="dropdown-item" href="tenant_profile.html">Profile</a></li>
-                  <li><a class="dropdown-item" href="tenant_settings.html">Settings</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="tenant_login.html">Sign out</a></li>
-                </ul>
-              </div-->
-
-
-
-              </div>
-          </div>
-      </header>
+ 
+  </header>
 

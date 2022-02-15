91fa588b2c880c940ae5be965c7eff789d79d322
Alondra Borja <alondracorbillonborjaborja14.ab@gmail.com>
2022-02-16 06:19:37 +0800

    for the zoom-meeting

---
+++ zoom-meeting/config.php
@@ -0,0 +1,14 @@
+<!-- brgyhealthcenterapp zoom app -->
+<!-- define('API_KEY','qkZhQ7xOTi6El3Bqv1V3Zg');
+define('API_SECRET','LmVLuQSY35ph8SapzapdS4BPreRHJx0Ho4tf');
+define('EMAIL_ID','brgyhealthcenterapp@gmail.com'); -->
+
+<!-- alondra borja zoom app -->
+<!-- define('API_KEY','2OjyxxKWQ3-uqPithP8elg');
+define('API_SECRET','ioUshqAwDzKTvlKdrOgadLk22z8sClKPasru');
+define('EMAIL_ID','alondracorbillonborja@gmail.com'); -->
+<?php
+define('API_KEY','2OjyxxKWQ3-uqPithP8elg');
+define('API_SECRET','ioUshqAwDzKTvlKdrOgadLk22z8sClKPasru');
+define('EMAIL_ID','alondracorbillonborja@gmail.com');
+?>
---
+++ zoom-meeting/vc_popup.php
@@ -0,0 +1,20 @@
+<?php
+include('config.php');
+include('api.php');
+$arr['topic']='Test by Brgy Health Center App';
+$arr['start_date']=date('2022-02-15 00:07:00');
+$arr['duration']=30;
+$arr['password']='zoom-pass';
+$arr['type']='2';
+$result=createMeeting($arr);
+
+if(isset($result->id)){
+	echo "Join URL: <a href='".$result->join_url."'>".$result->join_url."</a><br/>";
+	echo "Password: ".$result->password."<br/>";
+	echo "Start Time: ".$result->start_time."<br/>";
+	echo "Duration: ".$result->duration."<br/>";
+}else{
+	echo '<pre>';
+	print_r($result);
+}
+?>

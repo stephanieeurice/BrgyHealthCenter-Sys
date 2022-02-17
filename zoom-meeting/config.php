ae2ba01e3997701f3d05579b6114bd6dc1cb404c
Alondra Borja <92633379+alondracborja@users.noreply.github.com>
2022-02-17 17:25:27 +0800

    config file edited

--- zoom-meeting/config.php
+++ zoom-meeting/config.php
@@ -8,7 +8,7 @@ define('EMAIL_ID','brgyhealthcenterapp@gmail.com'); -->
 define('API_SECRET','ioUshqAwDzKTvlKdrOgadLk22z8sClKPasru');
 define('EMAIL_ID','alondracorbillonborja@gmail.com'); -->
 <?php
-define('API_KEY','2OjyxxKWQ3-uqPithP8elg');
-define('API_SECRET','ioUshqAwDzKTvlKdrOgadLk22z8sClKPasru');
-define('EMAIL_ID','alondracorbillonborja@gmail.com');
+    define('API_KEY','qkZhQ7xOTi6El3Bqv1V3Zg');
+    define('API_SECRET','LmVLuQSY35ph8SapzapdS4BPreRHJx0Ho4tf');
+    define('EMAIL_ID','brgyhealthcenterapp@gmail.com');
 ?>
